075d0295b882d546db3ecfb55ad6539d6fb4f0ed
nidodaj <nikoll.dodaj@eng.it>
2024-11-12 20:48:59 +0100

    init

---
+++ src/main/java/al/dooda/sql/LearningSqlApplication.java
@@ -0,0 +1,13 @@
+package al.dooda.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/al/dooda/sql/LearningSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package al.dooda.sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
