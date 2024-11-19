56d2966a0e13f065547bbb060c054f7785a945fd
nidodaj <nikoll.dodaj@eng.it>
2024-11-19 22:22:09 +0100

    ddl - use & rename

---
+++ src/main/java/al/dooda/sql/ddl/RENAME.sql
@@ -0,0 +1,7 @@
+/**
+
+  The RENAME command in SQL is used to rename database objects like tables, columns, or other schema objects.
+  The syntax and support for RENAME vary depending on the database management system (DBMS) being used.
+ */
+
+ALTER TABLE PRIVATE.TENAGERS RENAME TO PRIVATE.RENAMED_TABLE;
---
+++ src/main/java/al/dooda/sql/ddl/USE.sql
@@ -0,0 +1,6 @@
+/**
+  The USE command in SQL is used to select a specific database within a database management system (DBMS).
+  Once a database is selected, all subsequent SQL commands are executed within the context of that database.
+ */
+
+USE database_name;
