52f9395b6e6b4a9f0c96eefcdef15adfdea9a8fc
ndodaj <ndodaj@outlook.com>
2024-12-01 19:24:23 +0100

    test

--- src/main/java/al/dooda/sql/subqueries/column_subqueries.sql
+++ src/main/java/al/dooda/sql/subqueries/column_subqueries.sql
@@ -3,4 +3,5 @@
   A column serves as a structure that stores a specific type of data (ints, str, bool, etc.) in a table.
   Each column in a table is designed with a type, which configures the data that it can hold.
   Using the right column types and size can help to maintain data integrity and optimize performance.
+  test
  */
